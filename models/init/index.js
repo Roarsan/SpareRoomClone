16ad785b37d60d34b4fbe2e340f9a9a046d780ea
Roshan Gauchan <rgauchan3@gmail.com>
2025-09-26 02:38:11 +0100

    Refactored application structure  for MVC
    - Introduced a new database connection module in config/database.js
    - Created a list controller in controllers/listController.js to handle listing retrieval.
    - Updated app.js to utilize the new database connection and routing structure.
    - Added listRoutes.js for handling listing-related routes.

--- models/init/index.js
+++ models/init/index.js
@@ -1,20 +1,11 @@
 //connecting the db and seeding the sample data
-const db = require("mongoose");
 const initData = require("./sampleData.js");
 const ListModel = require("../listModel.js");
+// Import database connection
+const connectDB = require("../../config/database");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/spare_room";
-
-async function main() {
-    try {
-      await db.connect(MONGO_URL);
-      console.log("db connected");
-    } catch (err) {
-      console.error("Error connecting to DB:", err);
-    }
-  }
-  
-  main();
+// Connect to database
+connectDB();
 
 async function initDb(){
 await ListModel.deleteMany();
