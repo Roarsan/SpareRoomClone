9cf624202be74b94d08f27b8f070991330b6e044
Roshan Gauchan <rgauchan3@gmail.com>
2025-10-03 12:42:31 +0100

    Add listing detail view and update routes
    - Implemented a new route in app.js to show individual listings.
    - Added showListing method in listController.js to fetch and render a single listing.
    - Updated index.ejs to link each listing title to its detail view.

--- controllers/listController.js
+++ controllers/listController.js
@@ -5,11 +5,21 @@ const listController = {
   getAllListings: async (req, res) => {
     try {
       const listings = await ListModel.find();
-      res.render('listings/index', { listings });
+      res.render('listings/listings', { listings });
     } catch (error) {
       console.error('Error fetching listings:', error);
       res.status(500).send('Error fetching listings');
     }
+  },
+  // Get a single listing
+  showListing: async (req, res) => {
+   try { 
+    const list = await ListModel.findById(req.params.id);
+    res.render('listings/listDetail', { list });
+  } catch (error){
+    console.error('Error fetching listing:', error);
+    res.status(500).send('Error fetching listing');
+  }
   }
 };
 
