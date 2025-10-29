abd12902873f2d0936fd771d9c434e429981d88b
Roshan Gauchan <rgauchan3@gmail.com>
2025-10-29 01:18:58 +0000

    Add database initialization script for seeding sample data
    - Created initDB.js to connect to the database and seed it with sample data.
    - Implemented functionality to delete existing entries and insert new sample data from initData.
    - Logged a confirmation message upon successful initialization.

---
+++ database/initDB.js
@@ -0,0 +1,17 @@
+//connecting the db and seeding the sample data
+const initData = require("../models/init/sampleData.js");
+const ListModel = require("../models/listModel.js");
+
+// Import database connection
+const connectDB = require("../config/connectDB.js");
+
+// Connect to database
+connectDB();
+
+async function initDB(){
+await ListModel.deleteMany();
+await ListModel.insertMany(initData.data);
+console.log("sample data initialized");
+}
+
+initDB();
